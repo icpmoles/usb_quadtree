7d09a6bc4527a748342d5fa70350b5dfc40d0702
Iacopo Moles <iacopo@icpmol.es>
2026-09-03 11:03:56 +0200

    added support for csv/tsv linked to external animated version

--- extract_vid.py
+++ extract_vid.py
@@ -1,6 +1,8 @@
 from pathlib import Path
 from lark import Lark, Transformer
 import json
+import csv
+
 
 class VendorTransformer(Transformer):
     def start(self, items):
@@ -44,10 +46,10 @@ all_vendors = {}
 full_list = {}
 
 for vendor in data:
-    print(vendor)
+    # print(vendor)
     all_vendors[vendor["id"]] = vendor["name"]
     full_list[vendor["id"]] = vendor
-print(all_vendors)
+# print(all_vendors)
 
 
 with open("public/vendors.json", "w", encoding="utf-8") as f:
@@ -55,3 +57,17 @@ with open("public/vendors.json", "w", encoding="utf-8") as f:
 
 with open("full_list.json", "w", encoding="utf-8") as f:
     json.dump(full_list, f, indent=2, ensure_ascii=False)
+
+
+with open("vendors.tsv", "w", newline="") as f:
+    csv_writer = csv.writer(f, delimiter="\t", quotechar="'", quoting=csv.QUOTE_MINIMAL)
+    csv_writer.writerow(["vendor", "vid", "msb_hex", "lsb_hex", "msb_dec", "lsb_dec"])
+
+    for vid, l_vendor in all_vendors.items():
+        msb = vid[0:2]
+        lsb = vid[2:4]
+
+        msb_dec = int(msb, 16)
+        lsb_dec = int(lsb, 16)
+
+        csv_writer.writerow([l_vendor, vid, msb, lsb, msb_dec, lsb_dec])
